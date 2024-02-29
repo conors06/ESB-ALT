fd7c16f67f984faedf5ec5c457df667c7fe0ccd7
Conor Storey <conorwstorey@gmail.com>
2024-02-29 23:38:19 +0000

    1

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import json
 from datetime import datetime, timedelta
+import os
 
-with open('C:/Users/Conor/Documents/Coding/Project/scripts/json_data.json') as file:
-    data = json.load(file)
 
+
+try:
+    with open('json_data.json') as file:
+        data = json.load(file)
+except FileNotFoundError:
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_directory, 'json_data.json')
+    with open(file_path) as file:
+        data = json.load(file)
+
+#daystoshow = int(input("Enter number of days to show: "))
 df = pd.DataFrame(data)
 df.drop(columns=['MPRN', 'Read Type' ,'Meter Serial Number'], inplace=True)
 df['Read Date and End Time'] = pd.to_datetime(df['Read Date and End Time'])
+
 print(df)
 #print(df.columns)
