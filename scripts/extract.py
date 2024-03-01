882b1e0fd1a537f3197c63ee4c4dc2d94bedd521
Conor Storey <conorwstorey@gmail.com>
2024-03-01 18:52:43 +0000

    1

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import json
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time, date
 import os
 import tkinter as tk
 from tkcalendar import Calendar, DateEntry
+import sv_ttk
 
+# Load JSON data
 try:
     with open('json_data.json') as file:
         data = json.load(file)
@@ -14,14 +16,14 @@ except FileNotFoundError:
     with open(file_path) as file:
         data = json.load(file)
 
-
+# Create Tkinter GUI
 root = tk.Tk()
 root.title("Date Range Selector")
 
-
+# Function to calculate and display kW usage
 def calculate_kW_usage():
-    start_date = datetime.combine(cal_start.get_date(), datetime.min.time())
-    end_date = datetime.combine(cal_end.get_date(), datetime.min.time()) + timedelta(days=1)
+    start_date = datetime.combine(cal_start.get_date(), time.min)
+    end_date = datetime.combine(cal_end.get_date(), time.min) + timedelta(days=1)
     
     df = pd.DataFrame(data)
     df.drop(columns=['MPRN', 'Read Type', 'Meter Serial Number'], inplace=True)
@@ -32,13 +34,19 @@ def calculate_kW_usage():
     wantedkw_sum = df.loc[date_range, 'Read Value'].sum().round()
     result_label.config(text="Your kW usage is: {} KW".format(wantedkw_sum))
 
-
-cal_start = DateEntry(root, width=12, background='darkblue', foreground='white', date_pattern='dd/mm/yyyy')
-cal_end = DateEntry(root, width=12, background='darkblue', foreground='white', date_pattern='dd/mm/yyyy')
+# Create calendar date entry widgets
+cal_start = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2, date_pattern='dd/mm/yy')
+cal_end = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2, date_pattern='dd/mm/yy')
 cal_start.grid(row=0, column=0, padx=10, pady=10)
 cal_end.grid(row=0, column=1, padx=10, pady=10)
+
+# Create calculate button
 calculate_button = tk.Button(root, text="Calculate", command=calculate_kW_usage)
-calculate_button.grid(row=1, columnspan=2, padx=10, pady=10)
+calculate_button.grid(row=1, columnspan=2, padx=10, pady=10, )
+
+# Create result label
 result_label = tk.Label(root, text="")
 result_label.grid(row=2, columnspan=2, padx=10, pady=10)
+
+sv_ttk.set_theme("dark")
 root.mainloop()
