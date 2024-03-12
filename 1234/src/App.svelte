b4ca354fa74a3fbc5bab9c3f689fe75a9f2ce40a
Conor Storey <conorwstorey@gmail.com>
2024-03-12 22:31:10 +0000

    1

--- 1234/src/App.svelte
+++ 1234/src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-  import { fade, blur, scale, fly, slide } from 'svelte/transition';
-  import { quintOut } from 'svelte/easing';
-  import { LineChart } from 'lucide-svelte';
+  import { fade, blur, scale, fly, slide } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
+  import * as Drawer from "$lib/components/ui/drawer/index.js";
+  import * as Resizable from "$lib/components/ui/resizable";
+  import { LineChart } from "lucide-svelte";
   import CalendarIcon from "lucide-svelte/icons/calendar";
   import { Separator } from "$lib/components/ui/separator";
   import Sun from "lucide-svelte/icons/sun";
@@ -18,30 +20,30 @@
   import {
     CalendarDate,
     DateFormatter,
-    type DateValue
+    type DateValue,
   } from "@internationalized/date";
   import { cn } from "$lib/utils";
   import { Button } from "$lib/components/ui/button";
   import { RangeCalendar } from "$lib/components/ui/range-calendar";
   import * as Popover from "$lib/components/ui/popover";
-  import { onMount } from 'svelte';
-  import Plotly from 'plotly.js-basic-dist';
+  import { onMount } from "svelte";
+  import Plotly from "plotly.js-basic-dist";
 
   let totalKw: number | null = null;
   let chart_data: number | null = null;
-  let startDate: string = '';
-  let endDate: string = '';
+  let startDate: string = "";
+  let endDate: string = "";
   let chartInstance: Plotly.PlotlyHTMLElement;
   let showModal = false;
   let isTyping = false; // Add a flag for typing animation
 
   const df = new DateFormatter("en-US", {
-    dateStyle: "medium"
+    dateStyle: "medium",
   });
 
   let value: DateRange | undefined = {
     start: new CalendarDate(2022, 5, 16),
-    end: new CalendarDate(2022, 5, 16).add({ days: 20 })
+    end: new CalendarDate(2022, 5, 16).add({ days: 20 }),
   };
 
   let startValue: DateValue | undefined = undefined;
@@ -64,9 +66,9 @@
   const handleFormSubmit = async (event: Event) => {
     event.preventDefault();
 
-    emailInput = document.getElementById('email') as HTMLInputElement;
-    mprnInput = document.getElementById('mprn') as HTMLInputElement;
-    passwordInput = document.getElementById('current') as HTMLInputElement;
+    emailInput = document.getElementById("email") as HTMLInputElement;
+    mprnInput = document.getElementById("mprn") as HTMLInputElement;
+    passwordInput = document.getElementById("current") as HTMLInputElement;
 
     console.log(emailInput.value);
     console.log(mprnInput.value);
@@ -81,15 +83,15 @@
       email: emailInput.value,
       password: passwordInput.value,
       startTime: startDate, // Use the module-level variable
-      endTime: endDate // Use the module-level variable
+      endTime: endDate, // Use the module-level variable
     };
 
     const response = await fetch("http://127.0.0.1:5000/", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
 
     if (response.ok) {
@@ -98,16 +100,16 @@
       console.log(result);
       // Handle the result as needed
     } else {
-      console.error('Error:', response.status);
+      console.error("Error:", response.status);
       // Handle the error
     }
 
-    const response2 = await fetch('http://localhost:5000/chart', {
+    const response2 = await fetch("http://localhost:5000/chart", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
 
     if (response2.ok) {
@@ -116,7 +118,7 @@
       console.log(chart_data);
       // Handle the result as needed
     } else {
-      console.error('Error:', response2.status);
+      console.error("Error:", response2.status);
       // Handle the error
     }
   };
@@ -128,23 +130,23 @@
   }
 
   $: {
-    if (chart_data !== null && typeof chart_data === 'object') {
+    if (chart_data !== null && typeof chart_data === "object") {
       const { data, labels } = chart_data;
 
       const trace: Partial<Plotly.ScatterData> = {
         x: labels,
         y: data,
-        type: 'scatter',
-        mode: 'lines',
+        type: "scatter",
+        mode: "lines",
       };
 
       const layout = {
-        title: 'Energy Usage',
+        title: "Energy Usage",
         xaxis: {
-          title: 'Time',
+          title: "Time",
         },
         yaxis: {
-          title: 'Energy Usage (kW)',
+          title: "Energy Usage (kW)",
         },
       };
 
@@ -152,31 +154,67 @@
         responsive: true,
       };
 
-      Plotly.newPlot('chart', [trace], layout, config).then((instance) => {
+      Plotly.newPlot("chart", [trace], layout, config).then((instance) => {
         chartInstance = instance;
       });
     }
   }
 </script>
 
-<div class="navbar bg-white dark:bg-black" in:fade={{ delay: 100, duration: 500 }}>
-  <a class="btn btn-ghost btn rounded-full text-lg dark:text-white"
-     style="border-color: black; background-color: black; color: white; margin-right: 10px;"
-     href="https://example.com"
-     on:mouseenter={(e) => e.currentTarget.classList.add('animate-pulse')}
-     on:mouseleave={(e) => e.currentTarget.classList.remove('animate-pulse')}>
-    Example Link 1
-  </a>
-  <a class="btn btn-ghost btn rounded-full text-lg dark:text-white"
-     style="border-color: black; background-color: black; color: white;"
-     href="https://example.org"
-     on:mouseenter={(e) => e.currentTarget.classList.add('animate-pulse')}
-     on:mouseleave={(e) => e.currentTarget.classList.remove('animate-pulse')}>
-    Example Link 2
-  </a>
-</div>
+<Drawer.Root>
+  <Drawer.Trigger asChild let:builder>
+    <div class="text-center">
+    <Button   
+      builders={[builder]}
+      variant="outline"
+      class="rounded-full mt-6 mx-auto inline-block"
+    >
+      Open Drawer
+    </Button>
+  </Drawer.Trigger>
+  <Drawer.Content>
+    <div class="mx-auto w-full max-w-8xl">
+      <Drawer.Header>
+        <Drawer.Title>Docs</Drawer.Title>
+        <Drawer.Description>More Docs.</Drawer.Description>
+      </Drawer.Header>
+      <Resizable.PaneGroup
+        direction="horizontal"
+        class="min-h-[600px] max-h-[600px] max-w-8xl rounded-lg border"
+      >
+        <Resizable.Pane defaultSize={50}>
+          <div class="relative h-full overflow-hidden">
+            <div class="absolute inset-0 overflow-auto">
+              <img src="image.png" alt="Source" class="w-full h-auto" />
+            </div>
+          </div>
+        </Resizable.Pane>
+        <Resizable.Handle withHandle />
+        <Resizable.Pane defaultSize={50}>
+          <div class="relative h-full overflow-hidden">
+            <div class="absolute inset-0 overflow-auto">
+              <iframe
+                src="https://docs.google.com/document/d/e/2PACX-1vS_56PrCnlvJ_DV_MZMZ9vkGubaKtPx0lUeiGN6_ctRUNMkUEXCCXByO4OmGlJ02F1TEPDG2e6taIa_/pub?embedded=true"
+                class="w-full h-full"
+                title="Docs"
+              ></iframe>
+            </div>
+          </div>
+        </Resizable.Pane>
+      </Resizable.PaneGroup>
+      <Drawer.Footer>
+        <Drawer.Close asChild let:builder>
+          <Button builders={[builder]} variant="outline">Cancel</Button>
+        </Drawer.Close>
+      </Drawer.Footer>
+    </div>
+  </Drawer.Content>
+</Drawer.Root>
 
-<div style="position: absolute; top: 30px; right: 10px; padding: 10px;" in:slide={{ duration: 500, easing: quintOut }}>
+<div
+  style="position: absolute; top: 30px; right: 10px; padding: 10px;"
+  in:slide={{ duration: 500, easing: quintOut }}
+>
   <div class="space-y-1">
     <h4 class="text-sm font-medium leading-none">ESB Calculator</h4>
     <p class="text-sm text-muted-foreground">
@@ -186,7 +224,10 @@
 </div>
 
 <div class="flex flex-col items-center justify-center min-h-screen relative">
-  <div class="absolute top-4 right-4" in:scale={{ delay: 200, duration: 500, easing: quintOut }}>
+  <div
+    class="absolute top-4 right-4"
+    in:scale={{ delay: 200, duration: 500, easing: quintOut }}
+  >
     <Button on:click={toggleMode} variant="outline" size="icon">
       <Sun
         class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
@@ -199,43 +240,41 @@
   </div>
   <div class="grid gap-4">
     <Popover.Root openFocus>
-    <Popover.Trigger asChild let:builder>
-      <Button
-        variant="outline"
-        class={cn(
-          "w-[300px] justify-start text-left font-normal",
-          !value && "text-muted-foreground"
-        )}
-        builders={[builder]}
-      >
-        <CalendarIcon class="mr-2 h-4 w-4" />
-        {#if value && value.start}
-          {#if value.end}
-            {df.format(value.start.toDate(getLocalTimeZone()))} - {df.format(
-              value.end.toDate(getLocalTimeZone())
-            )}
+      <Popover.Trigger asChild let:builder>
+        <Button
+          variant="outline"
+          class={cn(
+            "w-[300px] justify-start text-left font-normal",
+            !value && "text-muted-foreground"
+          )}
+          builders={[builder]}
+        >
+          <CalendarIcon class="mr-2 h-4 w-4" />
+          {#if value && value.start}
+            {#if value.end}
+              {df.format(value.start.toDate(getLocalTimeZone()))} - {df.format(
+                value.end.toDate(getLocalTimeZone())
+              )}
+            {:else}
+              {df.format(value.start.toDate(getLocalTimeZone()))}
+            {/if}
+          {:else if startValue}
+            {df.format(startValue.toDate(getLocalTimeZone()))}
           {:else}
-            {df.format(value.start.toDate(getLocalTimeZone()))}
+            Pick a date
           {/if}
-        {:else if startValue}
-          {df.format(startValue.toDate(getLocalTimeZone()))}
-        {:else}
-          Pick a date
-        {/if}
-      </Button>
-    </Popover.Trigger>
-    <Popover.Content class="w-auto p-0" align="start">
-      <RangeCalendar
-        bind:value
-        bind:startValue
-        initialFocus
-        numberOfMonths={2}
-        placeholder={value?.start}
-      />
-    </Popover.Content>
-  </Popover.Root>
-  
-    
+        </Button>
+      </Popover.Trigger>
+      <Popover.Content class="w-auto p-0" align="start">
+        <RangeCalendar
+          bind:value
+          bind:startValue
+          initialFocus
+          numberOfMonths={2}
+          placeholder={value?.start}
+        />
+      </Popover.Content>
+    </Popover.Root>
 
     <Tabs.Root value="account" class="w-[400px]">
       <Tabs.List class="grid w-full grid-cols-2">
@@ -278,22 +317,30 @@
               <Label for="current">Current password</Label>
               <Input id="current" type="password" />
             </div>
-            
           </Card.Content>
           <Card.Footer>
             <Button on:click={handleFormSubmit}>Submit</Button>
-          </Card.Footer>          
+          </Card.Footer>
         </Card.Root>
       </Tabs.Content>
     </Tabs.Root>
     {#if totalKw !== null}
       <Alert.Root>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512" {...$$props}>
-          <path fill="#fdb436" d="m479.23 258.78l-197.7-36.571c-4.891-.905-7.09-6.667-4.044-10.6L427.065 18.432c4.498-5.808-2.366-13.542-8.667-9.766L21.336 246.595c-5.216 3.126-3.84 11.036 2.126 12.216l180.068 35.606c4.694.928 6.897 6.365 4.173 10.299L77.05 493.386c-4.05 5.849 2.684 13.111 8.821 9.513l395.511-231.856c5.308-3.111 3.899-11.144-2.152-12.263" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 512 512"
+          {...$$props}
+        >
+          <path
+            fill="#fdb436"
+            d="m479.23 258.78l-197.7-36.571c-4.891-.905-7.09-6.667-4.044-10.6L427.065 18.432c4.498-5.808-2.366-13.542-8.667-9.766L21.336 246.595c-5.216 3.126-3.84 11.036 2.126 12.216l180.068 35.606c4.694.928 6.897 6.365 4.173 10.299L77.05 493.386c-4.05 5.849 2.684 13.111 8.821 9.513l395.511-231.856c5.308-3.111 3.899-11.144-2.152-12.263"
+          />
         </svg>
         <Alert.Title>Total kW Usage</Alert.Title>
         <Alert.Description>
-            Your total kW usage for the selected date range is: {totalKw}
+          Your total kW usage for the selected date range is: {totalKw}
         </Alert.Description>
       </Alert.Root>
     {/if}
@@ -301,15 +348,3 @@
 </div>
 
 <div id="chart"></div>
-<style>
-  .navbar {
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-  }
-
-  .btn {
-    padding: 8px 16px;
-    font-size: 14px;
-  }
-</style>
--- 1234/src/Drawer.svelte
+++ 1234/src/Drawer.svelte
@@ -1,45 +1,47 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button/index.js";
-    import * as Drawer from "$lib/components/ui/drawer/index.js";
-    import * as Resizable from "$lib/components/ui/resizable";
-  
-   
-   
-  
-  </script>
-   
-  <Drawer.Root>
-    <Drawer.Trigger asChild let:builder>
-      <Button builders={[builder]} variant="outline">Open Drawer</Button>
-    </Drawer.Trigger>
-    <Drawer.Content>
-      <div class="mx-auto w-full max-w-sm">
-        <Drawer.Header>
-          <Drawer.Title>Docs</Drawer.Title>
-          <Drawer.Description>More Docs.</Drawer.Description>
-        </Drawer.Header>
-        <Resizable.PaneGroup
-    direction="horizontal"
-    class="min-h-[200px] max-w-md rounded-lg border"
-  >
-    <Resizable.Pane defaultSize={50}>
-      <div class="flex h-full items-center justify-center p-6">
-        <span class="font-semibold">Sidebar</span>
-      </div>
-    </Resizable.Pane>
-    <Resizable.Handle withHandle />
-    <Resizable.Pane defaultSize={50}>
-      <div class="flex h-full items-center justify-center p-6">
-        <span class="font-semibold">Content</span>
-      </div>
-    </Resizable.Pane>
-  </Resizable.PaneGroup>
-        <Drawer.Footer>
-          <Drawer.Close asChild let:builder>
-            <Button builders={[builder]} variant="outline">Cancel</Button>
-          </Drawer.Close>
-        </Drawer.Footer>
-    </Drawer.Content>
-  </Drawer.Root>
-  
-  
+  import { Button } from "$lib/components/ui/button/index.js";
+  import * as Drawer from "$lib/components/ui/drawer/index.js";
+  import * as Resizable from "$lib/components/ui/resizable";
+</script>
+
+<Drawer.Root>
+  <Drawer.Trigger asChild let:builder>
+    <Button builders={[builder]} variant="outline">Open Drawer</Button>
+  </Drawer.Trigger>
+  <Drawer.Content>
+    <div class="mx-auto w-full max-w-8xl">
+      <Drawer.Header>
+        <Drawer.Title>Docs</Drawer.Title>
+        <Drawer.Description>More Docs.</Drawer.Description>
+      </Drawer.Header>
+      <Resizable.PaneGroup
+        direction="horizontal"
+        class="min-h-[600px] max-h-[600px] max-w-8xl rounded-lg border"
+      >
+      <Resizable.Pane defaultSize={50}>
+        <div class="relative h-full overflow-hidden">
+          <div class="absolute inset-0 overflow-auto">
+            <img src="image.png" alt="Source" class="w-full h-auto" />
+          </div>
+        </div>
+      </Resizable.Pane>
+        <Resizable.Handle withHandle />
+        <Resizable.Pane defaultSize={50}>
+          <div class="relative h-full overflow-hidden">
+            <div class="absolute inset-0 overflow-auto">
+              <iframe
+                src="https://docs.google.com/document/d/e/2PACX-1vS_56PrCnlvJ_DV_MZMZ9vkGubaKtPx0lUeiGN6_ctRUNMkUEXCCXByO4OmGlJ02F1TEPDG2e6taIa_/pub?embedded=true"
+                class="w-full h-full" title="Docs"
+              ></iframe>
+            </div>
+          </div>
+        </Resizable.Pane>
+      </Resizable.PaneGroup>
+      <Drawer.Footer>
+        <Drawer.Close asChild let:builder>
+          <Button builders={[builder]} variant="outline">Cancel</Button>
+        </Drawer.Close>
+      </Drawer.Footer>
+    </div>
+  </Drawer.Content>
+</Drawer.Root>
